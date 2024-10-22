99d0667c89f943802d8596824b73d8138a2fbdb3
Ivan Perez <pumi0751@gmail.com>
2024-10-22 15:19:41 +0000

    feat: add button and change card size

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const getRandomSuit = () => suits[Math.floor(Math.random() * suits.length)];
 const getRandomNumber = () => numbers[Math.floor(Math.random() * numbers.length)]
 
 
+//#region "Random card"
 // Asign a random suit at the card
 const setRandomCard = () => {
 
@@ -27,7 +28,51 @@ const setRandomCard = () => {
         number.classList.add("red");
     }
 }
+//#endregion
+//#region "Button"
+const button = document.createElement("button");
+        button.innerText = "Generar otra carta";
+        button.id = "generateButton";
+        
+        // Añadir el botón al contenedor
+        const buttonContainer = document.getElementById("buttonContainer");
+        buttonContainer.appendChild(button);
+        
+        // Añadir evento de clic al botón para generar una nueva carta
+        button.addEventListener("click", function() {
+            setRandomCard();  // Genera una nueva carta al hacer clic en el botón
+        });
 
+// Add event click at the button
+button.addEventListener("click", function(){
+    setRandomCard(); // Generate new random card
+});
+//#endregion
+
+//#region "Change width and height"
+const widthInput = document.getElementById("widthInput");
+const heightInput = document.getElementById("heightInput");
+
+const updateCardSize = () =>{
+    const card = document.getElementById("card");
+    const newWidth = widthInput.value;
+    const newHeigth = heightInput.value;
+
+
+    if(newWidth){
+        card.style.width = `${newWidth}px`;
+    }
+    
+    if(newHeigth){
+        card.style.height = `${newHeigth}px`;
+    }
+
+    
+}
+//#endregion
+
+    widthInput.addEventListener("change", updateCardSize);
+    heightInput.addEventListener("change", updateCardSize);
 
 window.onload = function(){
     setRandomCard();
